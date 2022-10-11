39fc65f5a0a1dd26b94c0804a253be2d87f863ac
solopvova <solopvova1@gmail.com>
2022-10-11 19:50:09 +0700

    initial

--- server/app.js
+++ server/app.js
@@ -5,26 +5,28 @@ const cors = require('cors');
 const sequelize = require('./dbConnection');
 const app = express();
 
-//CORS
 app.use(cors({ credentials: true, origin: true }));
-
 app.use(express.json());
 
 
-app.get('/ping', (req, res) => {
-  res.json(['djinni', 'helper']);
+app.post('/mark', async (req, res) => {
+  const link = req.body.link;
+  const [hide, fav] = req.body.action === 'hide' ? [1,0] : [0,1];
+  const sql = "INSERT INTO `vacancy` (`link`, `hide`, `fav`) VALUES (:link, :hide, :fav);"
+  const inRes = await sequelize.query(sql, { replacements: { link, hide, fav }, type: sequelize.QueryTypes.INSERT })
+  res.json(inRes);
 });
 
-app.get('/hide', async (req, res) => {
-  // const words = req.body.words;
-  const vacancies = await sequelize.query("select count(*) from vacancy;", { type: sequelize.QueryTypes.SELECT })
-  console.log(vacancies);
-  res.json(vacancies);
+app.post('/check', async (req, res) => {
+  const links = req.body.links;
+  const sql = "SELECT * FROM  `vacancy` WHERE `link` in (:links);"
+  const result = await sequelize.query(sql, { replacements: { links }, type: sequelize.QueryTypes.SELECT })
+  res.json(result);
 });
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.SERVER_PORT || 8082;
 
 app.listen(PORT, () => {
-  console.log('Server running on http://localhost:8080');
+  console.log('Server running on http://localhost:8082');
 });
